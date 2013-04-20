615def8f0248789ea5123d7f0b49cf3052acc464
enbake <enbake@ubuntu.(null)>
2013-04-20 11:31:51 +0530

    Route Index Page

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ TeamApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'blogs#index'
 
   # See how all your routes lay out with "rake routes"
 
