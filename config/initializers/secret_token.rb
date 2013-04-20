6ef4508c775291d565ff9fc23a9e7a47cc7a5cda
maheep <msingh@enbake.com>
2013-04-20 10:21:50 +0530

    intial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamApp::Application.config.secret_token = 'fee10e3ca022f51745ad345a1124ab499024ea7278c44173343abfba032a7fb191baf055d35a58fbfad13eeb784917b5a9ed6dfc4a9a5842403189ba78c971c1'
