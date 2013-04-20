80934f36eeb75e2151ddbc8002e358c66cae3398
enbake <enbake@ubuntu.(null)>
2013-04-20 13:57:59 +0530

    Attached pepperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :photo
+  has_attached_file :photo,
+                    :styles => { :micro => '40x36>', :thumb => '185x185>', :avatar => '200x200>' },
+                    :storage => :s3,
+                    :s3_credentials => AppConfig.s3_creds,
+                    :s3_permissions => "public-read",
+                    :path => "sitter_photos/:style/:id",
+                    :bucket => AppConfig.s3_bucket,
+                    :default_url => 'missing_:style.png'
+
   # attr_accessible :title, :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130420054206) do
+ActiveRecord::Schema.define(:version => 20130420070417) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -34,19 +34,23 @@ ActiveRecord::Schema.define(:version => 20130420054206) do
     t.string   "password"
     t.string   "name"
     t.string   "status"
-    t.decimal  "balance"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.decimal  "balance",                :precision => 10, :scale => 0
+    t.datetime "created_at",                                                            :null => false
+    t.datetime "updated_at",                                                            :null => false
+    t.string   "email",                                                 :default => "", :null => false
+    t.string   "encrypted_password",                                    :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
